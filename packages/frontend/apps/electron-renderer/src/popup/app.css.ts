ef7a82d6e640de577e9e42cd3b78a5f58e3a0524
pengx17 <pengxiao@outlook.com>
2025-04-16 09:19:14 +0000

    fix(core): adjust recording user-select none style (#11735)

--- packages/frontend/apps/electron-renderer/src/popup/app.css.ts
+++ packages/frontend/apps/electron-renderer/src/popup/app.css.ts
@@ -2,7 +2,6 @@ import { globalStyle, style } from '@vanilla-extract/css';
 
 globalStyle('html', {
   backgroundColor: 'transparent',
-  userSelect: 'none',
 });
 
 globalStyle('body', {
@@ -13,4 +12,5 @@ export const root = style({
   backgroundColor: 'transparent',
   width: '100%',
   height: '100%',
+  userSelect: 'none',
 });
