aae65d231c41c6a611809d2effcae710fc8aa391
fundon <fundon@pindash.io>
2025-04-24 16:04:45 +0000

    fix(editor): should toggle toolbar when creating connector via auto complete (#11862)
    Closes: [BS-2403](https://linear.app/affine-design/issue/BS-2403/[improvement]-edgeless-拖出-connector-时，yuan素的-toolbar-需要消失)

--- blocksuite/affine/blocks/root/src/edgeless/components/auto-complete/edgeless-auto-complete.ts
+++ blocksuite/affine/blocks/root/src/edgeless/components/auto-complete/edgeless-auto-complete.ts
@@ -27,6 +27,7 @@ import {
   ShapeElementModel,
   shapeMethods,
 } from '@blocksuite/affine-model';
+import { ToolbarRegistryIdentifier } from '@blocksuite/affine-shared/services';
 import { handleNativeRangeAtPoint } from '@blocksuite/affine-shared/utils';
 import { DisposableGroup } from '@blocksuite/global/disposable';
 import type { Bound, IVec } from '@blocksuite/global/gfx';
@@ -211,6 +212,8 @@ export class EdgelessAutoComplete extends WithDisposable(LitElement) {
           point,
           otherSideId ? [otherSideId] : []
         );
+
+        this.std.get(ToolbarRegistryIdentifier).flags.hide();
       }
     });
 
@@ -221,6 +224,8 @@ export class EdgelessAutoComplete extends WithDisposable(LitElement) {
         this.gfx.selection.clear();
         this._createAutoCompletePanel(e, connector);
       }
+
+      this.std.get(ToolbarRegistryIdentifier).flags.show();
 
       this._isMoving = false;
       this.connectionOverlay.clear();
