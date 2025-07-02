d768ad4af0fa912002467fb61ddd9ff76faa8cb1
L-Sun <zover.v@gmail.com>
2025-07-02 17:42:16 +0800

    feat(editor): block comment extension (#12980)
    #### PR Dependency Tree
    
    
    * **PR #12980** 👈
    
    This tree was auto-generated by
    [Charcoal](https://github.com/danerwilliams/charcoal)

--- blocksuite/affine/shared/src/services/comment-service/utils.ts
+++ blocksuite/affine/shared/src/services/comment-service/utils.ts
@@ -1,9 +1,45 @@
-import type { Store } from '@blocksuite/store';
+import { CommentIcon } from '@blocksuite/icons/lit';
+import { BlockSelection } from '@blocksuite/std';
+import type { BlockModel, Store } from '@blocksuite/store';
 
-import type { CommentId } from './comment-provider';
+import type { ToolbarAction } from '../toolbar-service';
+import { type CommentId, CommentProviderIdentifier } from './comment-provider';
 
 export function findCommentedBlocks(store: Store, commentId: CommentId) {
-  return store.getAllModels().filter(block => {
-    return 'comment' in block.props && block.props.comment === commentId;
+  type CommentedBlock = BlockModel<{ comments: Record<CommentId, boolean> }>;
+  return store.getAllModels().filter((block): block is CommentedBlock => {
+    return (
+      'comments' in block.props &&
+      typeof block.props.comments === 'object' &&
+      block.props.comments !== null &&
+      commentId in block.props.comments
+    );
   });
 }
+
+export const blockCommentToolbarButton: Omit<ToolbarAction, 'id'> = {
+  tooltip: 'Comment',
+  when: ({ std }) => !!std.getOptional(CommentProviderIdentifier),
+  icon: CommentIcon(),
+  run: ctx => {
+    const commentProvider = ctx.std.getOptional(CommentProviderIdentifier);
+    if (!commentProvider) return;
+    const selections = ctx.selection.value;
+
+    const model = ctx.getCurrentModel();
+
+    if (selections.length > 1) {
+      commentProvider.addComment(selections);
+    } else if (model) {
+      commentProvider.addComment([
+        new BlockSelection({
+          blockId: model.id,
+        }),
+      ]);
+    } else if (selections.length === 1) {
+      commentProvider.addComment(selections);
+    } else {
+      return;
+    }
+  },
+};
